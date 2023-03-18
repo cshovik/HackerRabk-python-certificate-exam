44fe267fcda355f88c3465e0cd0f6de51878f8a4
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2023-03-18 20:05:36 +0530

    Update python reverse word and swap case .py

--- python reverse word and swap case .py
+++ python reverse word and swap case .py
@@ -1,19 +1,8 @@
-#!/bin/python
-
 import math
 import os
 import random
 import re
 import sys
-
-
-
-#
-# Complete the 'reverse_words_order_and_swap_cases' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING sentence as parameter.
-#
 
 def reverse_words_order_and_swap_cases(sentence):
     word_list = sentence.split()
